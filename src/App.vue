377faa8f82e4a885ef8b71fc4e27285d72aa4fcd
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-08-14 10:36:24 +0200

    app.vue update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  
+  <div id="app">
+    <Header></Header>    
+    <router-view/>
+  </div>
 </template>
 
-<style scoped>
-
-</style>

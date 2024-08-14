0198b27261eadb8c48120a2dc159a44a2a505c3e
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-08-14 10:33:13 +0200

    header template

---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+git<script>
+
+</script>
+
+<template>
+
+  <nav class="container">
+      <div>
+           <img src="../assets/planet-5-logo.png" class="logo" alt="planet Logo">
+      </div>
+  </nav>
+</template>
+
+<style scoped>
+  .container {
+    background-color: #2b3843;
+    height: 72px;
+    border-radius: 10px;
+    padding: 5px;
+    display: flex;
+    align-items: center; 
+    justify-content: space-between; 
+  }
+
+  .logo {
+    height: 60px;
+    width: 150px;
+    margin-left: 20px;
+    border-radius: 5px;
+  }
+
+  .logo-text {
+    color: rgb(56, 97, 131);
+    font-size: 30px;
+    font-weight: 600;
+    margin-left: 20px;
+  }
+
+  
+
+  @media (max-width: 1200px) {
+    .logo {
+      height: 50px;
+      width: 120px;
+    }
+
+    .logo-text {
+      font-size: 26px;
+    }
+  }
+
+  @media (max-width: 992px) {
+    .logo {
+      height: 40px;
+      width: 100px;
+    }
+
+    .logo-text {
+      font-size: 22px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .container {
+      flex-direction: column; 
+      align-items: center; 
+      height: auto; 
+      padding: 10px; 
+    }
+
+    .logo {
+      height: 40px;
+      width: 100px;
+      margin: 10px 0; 
+    }
+
+    .logo-text {
+      font-size: 18px;
+      margin: 0; 
+    }
+  }
+</style>
